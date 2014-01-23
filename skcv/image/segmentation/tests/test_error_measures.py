2a9240785fcf57412d9b6f0efbd57e07e877e05a
Guillem Palou Visa <guillem.palou@gmail.com>
2014-01-23 16:59:02 +0100

    added a test for boundary detection error measure

--- skcv/image/segmentation/tests/test_error_measures.py
+++ skcv/image/segmentation/tests/test_error_measures.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy.testing import assert_equal
 from skcv.image.segmentation.error_measures import undersegmentation_error
+from skcv.image.segmentation.error_measures import boundary_detection
 
 def test_undersegmentation_error():
     N = 100
@@ -25,3 +26,25 @@ def test_undersegmentation_error():
     ue = undersegmentation_error(part, part_gt, tolerance=0)
 
     assert_equal(ue,0.5001)
+
+
+def test_boundary_detection():
+    n = 10
+    m = 10
+
+    part = np.zeros((n, m))
+
+    part[:n/2, :m/2] = 0
+    part[n/2:, :m/2] = 1
+    part[:n/2, m/2:] = 2
+    part[n/2:, m/2:] = 3
+
+    part_gt = part.copy()
+
+    part[n/2, m/4] = 0
+    part[n/4-1, m/2-1] = 2
+
+    precision, recall = boundary_detection(part, part_gt, 0)
+
+    assert_equal(precision, 0.75)
+    assert_equal(recall, 0.9)

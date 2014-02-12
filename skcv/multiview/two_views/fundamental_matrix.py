f45936a13af2e13afec456acdf6fa1de20d3466c
Guillem Palou Visa <guillem.palou@gmail.com>
2014-02-12 13:18:08 +0100

    adding fundamental matrix algorithm - not finished

--- skcv/multiview/two_views/fundamental_matrix.py
+++ skcv/multiview/two_views/fundamental_matrix.py
@@ -1,27 +1,74 @@
 import numpy as np
+from numpy.linalg import svd
 
-def eight_point_algorithm(x1, x2, max_iter=1000, inlier_threshold=2):
-    """ Computes the fundamental matrix using the eight point algorithm
-        (Hartley 1997)
+from skcv.multiview.util import euclidean_to_homogeneous
+from skcv.multiview.util import homogeneous_to_euclidean
+from skcv.multiview.util import normalize_points
 
-        Parameters
-        ----------
-        x1: numpy array
-        projections of points in the first image
 
-        x2: numpy array
-        projections of points in the second image
+def eight_point_algorithm(x1, x2):
+    """ Computes the fundamental matrix from 8 (or more) projection
+    point pairs
 
-        max_iter: int, optional
-        maximum number of iterations of the ransac algorithm
+    Parameters
+    ----------
+    x1: numpy array
+    projections of points in the first image, in homogeneous coordinates
 
-        inlier_threshold: float, optional
-        maximum distance to consider a point pair inlier
+    x2: numpy array
+    projections of points in the second image, in homogeneous coordinates
 
-        Returns
-        -------
-        F, the fundamental matrix satisfying x2.T * F * x1 = 0
+    Returns
+    -------
+    F, the fundamental matrix satisfying x2.T * F * x1 = 0
 
     """
 
-        while i < max_iter:
+    N = x1.shape[1]
+    if (x2.shape != N):
+        raise ValueError("Shape must be the same")
+
+    # normalize points
+    x1n, t1 = normalize_points(x1, is_homogeneous=True)
+    x2n, t2 = normalize_points(x2, is_homogeneous=True)
+
+    # build the vector
+    a = np.zeros((9, N))
+    for i in range(N):
+        a[:, i] = np.kron(x2n[:, i], x1n[:, i])
+
+    # find F in the normalized coordinates and transform it
+    u,d,v = svd(a.T)
+    F = np.reshape(v, (3, 3))
+    F = t2.T*F*t1
+
+    return F
+
+
+def robust_F_estimation(x1, x2, max_iter=1000, inlier_threshold=2):
+    """ Computes the fundamental matrix using the eight point algorithm
+    (Hartley 1997)
+
+    Parameters
+    ----------
+    x1: numpy array
+    projections of points in the first image
+
+    x2: numpy array
+    projections of points in the second image
+
+    max_iter: int, optional
+    maximum number of iterations of the ransac algorithm
+
+    inlier_threshold: float, optional
+    maximum distance to consider a point pair inlier
+
+    Returns
+    -------
+    F, the fundamental matrix satisfying x2.T * F * x1 = 0
+
+    """
+
+    iteration = 0
+    while iteration < max_iter:
+        pass
